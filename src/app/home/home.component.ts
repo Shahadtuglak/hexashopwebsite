8ab527d16ffa74a628a61ac723a686a37cd1e086
unknown <tuglakshahad@gmail.com>
2022-04-20 17:21:19 +0530

    add owl carousel to home menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 declare var $:any;
 
 @Component({
@@ -25,6 +27,32 @@ export class HomeComponent implements OnInit {
         }, 500);
       });
     });
+  }
+
+  // mens carousel
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: true
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
-// import { AboutComponent } from '../about/about.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 
 const homeRoutes:Routes = [
@@ -17,7 +17,8 @@ const homeRoutes:Routes = [
     HomeComponent
   ],
   imports: [
-    RouterModule.forChild(homeRoutes)
+    RouterModule.forChild(homeRoutes),
+    CarouselModule
   ]
 })
 export class HomeModule { }
